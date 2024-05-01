4845ccea8d9f603cad8903546356e6563a656878
Rizqy Syahputra <rizqysyahputra@192.168.1.5>
2024-05-01 21:12:26 +0700

    first commit

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,24 @@
+import StreamVideoProvider from '@/providers/StreamClientProvider'
+import { Metadata } from 'next';
+import React, { ReactNode } from 'react'
+
+export const metadata: Metadata = {
+  title: "ZOOM",
+  description: "Video calling app",
+  icons: {
+    icon: '/icons/logo.svg'
+  }
+};
+
+const RootLayout = ({ children }: Readonly<{ children: ReactNode }>) => {
+  return (
+    <main>
+      <StreamVideoProvider>
+        {children}
+      </StreamVideoProvider>
+      {children}
+    </main>
+  )
+}
+
+export default RootLayout
